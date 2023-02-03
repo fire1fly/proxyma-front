fc0ef24b6f30c1ac45820fcdf668de183d3ebc22
fire1fly <danyseidev@gmail.com>
2023-02-03 21:50:41 +0800

    feat: pages: check ip and anonimity

---
+++ js/copy.js
@@ -0,0 +1,18 @@
+const copyElList = document.querySelectorAll(".copy");
+
+copyElList.forEach(el => {
+  const copyValue = el.querySelector(".copy-value");
+  const copyTrigger = el.querySelector(".copy-trigger");
+  copyTrigger.addEventListener("click", () => {
+    const str = copyValue.innerText;
+    const field = document.createElement('textarea');
+    field.value = str;
+    field.setAttribute('readonly', '');
+    field.style.position = 'absolute';
+    field.style.left = '-9999px';
+    document.body.appendChild(field);
+    field.select();
+    document.execCommand('copy');
+    document.body.removeChild(field);
+  });
+});
