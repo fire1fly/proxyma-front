c387ec40b49e70e227d89e7c474ad77fd5513ef4
fire1fly <danyseidev@gmail.com>
2023-03-02 19:25:21 +0800

    account balance page

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,62 +1,79 @@
-class Dropdown {
-  constructor(el) {
-    this.$dd = el;
-    this.setup();
-  }
+document.addEventListener("DOMContentLoaded", function () {
+  class Dropdown {
+    constructor(el) {
+      this.$dd = el;
+      this.$input = el.querySelector(".dd-input");
+      this.$selectedItem = el.querySelector(".selected")
+      this.ddElems = el.querySelectorAll(".dd-item");
+      this.setup();
+    }
 
-  setup() {
-    this.handleClick = this.handleClick.bind(this);
-    this.handleOutClick = this.handleOutClick.bind(this);
-    this.$dd.addEventListener("click", this.handleClick);
-    document.addEventListener("click", this.handleOutClick);
-  }
+    setup() {
+      this.handleClick = this.handleClick.bind(this);
+      this.handleOutClick = this.handleOutClick.bind(this);
+      this.$dd.addEventListener("click", this.handleClick);
+      document.addEventListener("click", this.handleOutClick);
 
-  handleClick(e) {
-
-    if (this.$dd.classList.contains("select")) {
-      const selectedItem = this.$dd.querySelector(".selected"),
-        clickedItem = e.target.closest(".dd-item"),
-        input = this.$dd.querySelector(".dd-input");
-
-      if (clickedItem) {
-        selectedItem.innerHTML = clickedItem.innerHTML;
-        input.value = clickedItem.dataset.value;
-        input.dispatchEvent(new Event("change"));
+      if (this.$dd.classList.contains("select")) {
+        this.setInitState();
       }
+
     }
 
-    if (this.$dd.classList.contains("active")) {
-      this.close();
-    } else {
-      this.open();
+    setInitState() {
+      this.ddElems.forEach(elem => {
+        if (elem.dataset.value === this.$input.value) {
+          this.$selectedItem.innerHTML = elem.innerHTML;
+        }
+      });
     }
 
-  }
+    handleClick(e) {
 
-  handleOutClick(e) {
-    let path = e.path || (e.composedPath && e.composedPath());
-    if (path) {
-      if (!path.includes(this.$dd)) {
+      if (this.$dd.classList.contains("select")) {
+        const clickedItem = e.target.closest(".dd-item");
+
+        if (clickedItem) {
+          this.$selectedItem.innerHTML = clickedItem.innerHTML;
+          this.$input.value = clickedItem.dataset.value;
+          this.$input.dispatchEvent(new Event("change"));
+        }
+      }
+
+      if (this.$dd.classList.contains("active")) {
         this.close();
+      } else {
+        this.open();
       }
+
+    }
+
+    handleOutClick(e) {
+      let path = e.path || (e.composedPath && e.composedPath());
+      if (path) {
+        if (!path.includes(this.$dd)) {
+          this.close();
+        }
+      }
+    }
+
+    open() {
+      this.$dd.classList.add("active");
+    }
+
+    close() {
+      this.$dd.classList.remove("active");
+    }
+
+    toggle() {
+      this.$dd.classList.toggle("active");
     }
   }
 
-  open() {
-    this.$dd.classList.add("active");
-  }
+  const ddElems = document.querySelectorAll(".dd");
 
-  close() {
-    this.$dd.classList.remove("active");
-  }
+  ddElems && ddElems.forEach(el => {
+    const dd = new Dropdown(el);
+  });
 
-  toggle() {
-    this.$dd.classList.toggle("active");
-  }
-}
-
-const ddElems = document.querySelectorAll(".dd");
-
-ddElems && ddElems.forEach(el => {
-  const dd = new Dropdown(el);
 });
